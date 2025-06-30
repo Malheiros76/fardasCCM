d2d6a952d0e67fa2a98d1c84a2f37ee8dfe394e7
Malheiros76 <l_souza.malheiros@hotmail.com>
2025-06-30 10:18:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -392,4 +392,4 @@ else:
         st.session_state.logado = False
         st.session_state.pop('usuario_logado', None)
         st.session_state.pop('nivel_usuario', None)
-        st.experimental_rerun()
+        st.rerun()
